93396558912bb646babfd765001f326247b4f6e8
kazmiali123 <kazmiali123@gmail.com>
2023-12-10 04:08:03 -0500

    initial upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const Circle = require('./lib/circle');
+const Square = require('./lib/square');
+const Triangle = require('./lib/triangle');
+
+
